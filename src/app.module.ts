7f7270c293b2ed2eb759d034aaf96e48286f87e1
auddnjs2008 <auddnjs2008@naver.com>
2025-01-14 06:59:34 +0900

    response logging interceptor

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,9 +14,10 @@ import { UserModule } from './user/user.module';
 import { User } from './user/entities/user.entity';
 import { envVariableKeys } from './common/const/env.const';
 import { BearerTokenMiddleware } from './auth/middleware/bearer-token.middleware';
-import { APP_GUARD } from '@nestjs/core';
+import { APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
 import { AuthGuard } from './auth/guard/auth.guard';
 import { RBAGuard } from './auth/guard/rbac.guard';
+import { ResponseTimeInterceptor } from './common/interceptor/response-time.interceptor';
 
 @Module({
   imports: [
@@ -73,6 +74,10 @@ import { RBAGuard } from './auth/guard/rbac.guard';
     {
       provide:APP_GUARD,
       useClass:RBAGuard
+    },
+    {
+      provide:APP_INTERCEPTOR,
+      useClass: ResponseTimeInterceptor
     }
   ]
 
