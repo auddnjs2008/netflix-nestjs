8ed349f300aa5f8c6714da14f5f058e3594d694e
auddnjs2008 <auddnjs2008@naver.com>
2025-01-19 13:41:32 +0900

    Custom Logger

--- src/common/tasks.service.ts
+++ src/common/tasks.service.ts
@@ -6,16 +6,18 @@ import { join , parse} from "path";
 import { Movie } from "src/movie/entity/movie.entity";
 import { Repository } from "typeorm";
 import { Logger } from "@nestjs/common";
+import { DefaultLogger } from "./logger/default.logger";
 
 @Injectable()
 export class TasksService{
-    private readonly logger = new Logger(TasksService.name);
+    // private readonly logger = new Logger(TasksService.name);
 
 
     constructor(
         @InjectRepository(Movie)
         private readonly movieRepository:Repository<Movie>,
-        private readonly schedulerRegistry:SchedulerRegistry
+        private readonly schedulerRegistry:SchedulerRegistry,
+        private readonly logger:DefaultLogger
     ){}
 
     @Cron('*/5 * * * * *')
