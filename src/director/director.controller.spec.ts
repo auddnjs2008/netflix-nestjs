ba393de6486fe8b1cb36ab3a98ea5d06629204a6
auddnjs2008 <auddnjs2008@naver.com>
2025-01-26 11:21:16 +0900

    director test code

--- src/director/director.controller.spec.ts
+++ src/director/director.controller.spec.ts
@@ -1,20 +1,108 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { DirectorController } from './director.controller';
 import { DirectorService } from './director.service';
+import { Director } from './entity/director.entity';
+
+
+const mockDirectorService = {
+  create:jest.fn(),
+  findAll:jest.fn(),
+  findOne:jest.fn(),
+  update:jest.fn(),
+  remove:jest.fn()
+}
+
 
 describe('DirectorController', () => {
-  let controller: DirectorController;
+  let directorController: DirectorController;
+  let directorService:DirectorService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [DirectorController],
-      providers: [DirectorService],
+      providers: [
+        {
+          provide:DirectorService,
+          useValue:mockDirectorService
+        }
+      ],
     }).compile();
 
-    controller = module.get<DirectorController>(DirectorController);
+    directorController = module.get<DirectorController>(DirectorController);
+    directorService = module.get<DirectorService>(DirectorService);
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(directorController).toBeDefined();
   });
+
+  describe('findAll', () => {
+    it('should return all director', async () => {
+        const directors = [
+          {
+            id:1,
+            name:'kmw'
+          },{
+            id:2,
+            name:'kmw2'
+          }
+        ];
+      
+      jest.spyOn(directorService,'findAll').mockResolvedValue(directors as Director[]);
+      const result = await directorController.findAll();
+      expect(directorService.findAll).toHaveBeenCalled();
+      expect(result).toEqual(directors);
+    })
+  })
+
+  describe('findOne', () => {
+    it('should return a director', async () => {
+      const director = {id:1, name:"kmw"}
+      jest.spyOn(directorService,'findOne').mockResolvedValue(director as Director);
+      const result = await directorController.findOne(1);
+      expect(directorService.findOne).toHaveBeenCalledWith(1);
+      expect(result).toEqual(director);
+    })
+  })
+
+  describe('create', () => {
+    it('should create a director', async () => {
+      const director = {id:1, name:"kmw"}
+      const createDirectorDto = {
+        name:'kmw',
+        dob:new Date(),
+        nationality:'asdf'
+      };
+
+      jest.spyOn(directorService,'create').mockResolvedValue(director as Director);
+      const result = await directorController.create(createDirectorDto);
+      expect(directorService.create).toHaveBeenCalledWith(createDirectorDto);
+      expect(result).toEqual(director);
+    });
+  });
+
+  describe('update', () => {
+    it('should update a director', async () => {
+      const updateDirectorDto = {name:'kmw-fix'};
+      const id = 1;
+      const updatedDirector = {id:1,name:'kmw-fix'};
+
+      jest.spyOn(directorService,'update').mockResolvedValue(updatedDirector as Director);
+      const result = await directorController.update(1,updateDirectorDto);
+      expect(directorService.update).toHaveBeenCalledWith(id,updateDirectorDto);
+      expect(result).toEqual(updatedDirector);
+    })
+  })
+
+  describe('remove', () => {
+    it('should delete a director', async () => {
+      const id = 1;
+      jest.spyOn(directorService,'remove').mockResolvedValue(id);
+      const result = await directorController.remove(id);
+      expect(directorService.remove).toHaveBeenCalledWith(id);
+      expect(result).toEqual(id)
+    })
+  })
+
+
 });
--- src/director/director.service.spec.ts
+++ src/director/director.service.spec.ts
@@ -1,18 +1,136 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { DirectorService } from './director.service';
+import { Repository } from 'typeorm';
+import { Director } from './entity/director.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { CreateDirectorDto } from './dto/create-director.dto';
+import { NotFoundException } from '@nestjs/common';
+
+
+const mockDirectorRepository = {
+  save:jest.fn(),
+  find:jest.fn(),
+  findOne:jest.fn(),
+  update:jest.fn(),
+  delete:jest.fn()
+}
+
 
 describe('DirectorService', () => {
-  let service: DirectorService;
+  let directorService: DirectorService;
+  let directorRepository:Repository<Director>;
+
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [DirectorService],
+      providers: [
+       DirectorService,  
+      {
+        provide:getRepositoryToken(Director),
+        useValue:mockDirectorRepository
+      }],
     }).compile();
 
-    service = module.get<DirectorService>(DirectorService);
+    directorService = module.get<DirectorService>(DirectorService);
+    directorRepository = module.get<Repository<Director>>(getRepositoryToken(Director));
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(directorService).toBeDefined();
   });
+
+  describe('create', () => {
+
+    it('should create a new director', async () => {
+      const createDirectorDto = {
+         name:'kmw',
+         dob:new Date(),
+         nationality:'asdf'
+      };
+
+      jest.spyOn(mockDirectorRepository,'save').mockResolvedValue(createDirectorDto);
+      const result = await directorService.create(createDirectorDto)
+
+      expect(mockDirectorRepository.save).toHaveBeenCalledWith(createDirectorDto);
+      expect(result).toBe(createDirectorDto);
+    })
+  })
+
+  describe('findAll', () => {
+    it('should find all director', async () => {
+      const directors = [{id:1,name:'test'},{id:2,name:'test2'}];
+      jest.spyOn(mockDirectorRepository,'find').mockResolvedValue(directors);
+
+      const result = await directorService.findAll();
+      expect(result).toBe(directors);
+    })
+  })
+
+  describe('findOne', () => {
+    it('should find a director by id' ,async () => {
+      const director = {id:1,name:'test'};
+      jest.spyOn(mockDirectorRepository,'findOne').mockResolvedValue(director);
+
+      const result = await directorService.findOne(1);
+
+      expect(mockDirectorRepository.findOne).toHaveBeenCalledWith(
+        {where:{id:1}}
+      );
+      expect(result).toBe(director);
+    })
+  });
+
+  describe('update', () => {
+    it('should update a director by id', async () => {
+      const id = 1;
+      const updateDirectorDto = {name:'kmw'};
+      const director = {id:1,name:'test'};
+      jest.spyOn(mockDirectorRepository,'findOne').mockResolvedValueOnce(director);
+      jest.spyOn(mockDirectorRepository,'findOne').mockResolvedValueOnce({...director,...updateDirectorDto});
+
+      const result = await directorService.update(id,updateDirectorDto);
+      expect(mockDirectorRepository.findOne).toHaveBeenCalledWith({
+        where: {
+          id:1
+        }
+      });
+      expect(result).toEqual({...director,...updateDirectorDto});
+    })
+
+    it('should thorw a Error for not founding user', async () => {
+      const id = 1;
+      const updateDirectorDto = {name:'kmw'};
+      jest.spyOn(mockDirectorRepository,'findOne').mockResolvedValue(null);
+      expect(directorService.update(id,updateDirectorDto)).rejects.toThrow(NotFoundException);
+    })
+  });
+
+
+  describe('remove', () => {
+    it('should remove a director by id', async () => {
+      const id = 1;
+      const director = {id:1,name:'kmw'};
+      jest.spyOn(mockDirectorRepository,'findOne').mockResolvedValue(director);
+      
+      const result = await directorService.remove(id);
+      expect(mockDirectorRepository.findOne).toHaveBeenCalledWith({
+        where:{
+          id
+        }
+      });
+      expect(result).toBe(id);
+    })
+
+    it('should throw a error for not founding user', async () => {
+      const id = 1;
+      jest.spyOn(mockDirectorRepository,'findOne').mockResolvedValue(null);
+      expect(directorService.remove(id)).rejects.toThrow(NotFoundException);
+    })
+
+
+  })
+
+
+
+
 });
